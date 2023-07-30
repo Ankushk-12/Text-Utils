2d713f7d98f2214971ec90cf52f7374dae84d863
Ankushk-12 <Ankush.Zversal@gmail.com>
2023-07-30 17:42:54 +0530

    Push the code in the code to run good

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar(props) {
         >
           {/* bg-body-tertiary  */}
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="/">
               {props.title}
             </a>
             <button
@@ -32,7 +32,7 @@ export default function Navbar(props) {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" href="#">
+                  <a className="nav-link active" href="/">
                     Home
                   </a>
                 </li>
